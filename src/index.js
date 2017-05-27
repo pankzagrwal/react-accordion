7f0c6ef27f6c1096ce582a809908fa7d4f63e81c
Pankaj <pankzmail@gmail.com>
2017-05-27 14:01:12 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Accordion from './app/index.js';
+import AccordionContent from './app/accordionComponent.js';
+import './index.css';
+
+ReactDOM.render(<Accordion collapseAll = {false} defaultOpen = {"acc_1"}>
+				<AccordionContent title = "header One" id = "acc_0">
+					abc Content
+				</AccordionContent>
+				<AccordionContent title = "Header Two" id = "acc_1">
+					<p>
+						<a href = "#">Content Two</a>
+					</p>
+				</AccordionContent>
+
+				</Accordion>
+	, document.getElementById('root'));
